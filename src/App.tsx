6658eed6c68102f4078f9e09b5e37ddd346c6248
tictools <jaumevillarreal@gmail.com>
2023-02-11 19:55:40 +0100

    feat: implement views in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import ErrorRequest from "./components/ErrorRequest";
+import LoaderRequest from "./components/LoaderRequest";
+import useFetchRequest from "./hooks/useFetchRequest";
+import UsersList from "./views/UsersList";
 
-function App() {
+function App(): JSX.Element {
+  const requestStatus = useFetchRequest("https://reqres.in/api/users");
+
   return (
     <div className="App">
       <h1>Event Dispatcher Demo</h1>
+      <main>
+        <section>
+          <h2>Users List</h2>
+          {requestStatus.state === 'loading' && <LoaderRequest />}
+          {requestStatus.state === 'error' && <ErrorRequest error={requestStatus.error} />}
+          {requestStatus.state === 'success' && <UsersList users={requestStatus.data} />}
+        </section>
+      </main>
     </div>
   );
 }
