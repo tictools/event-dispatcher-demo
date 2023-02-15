b7a4430929fd0a1a379cc8db823703483e83825a
tictools <jaumevillarreal@gmail.com>
2023-02-15 07:40:33 +0100

    feat: fix prop type in Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,10 +2,10 @@ import styles from './Card.module.css'
 
 interface CardProps {
   header: string
-  children: JSX.Element
+  children: React.ReactNode
 }
 
-export default function Card ({ header, children }: CardProps) {
+export default function Card({ header, children }: CardProps) {
   return (
     <section>
       <h2>{header}</h2>
