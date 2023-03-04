e3dee736263eb90913ecee742af6f62df22e19a1
tictools <jaumevillarreal@gmail.com>
2023-03-04 08:52:38 +0100

    feat: remove log

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,8 @@ import { UsersApiResponse } from './types'
 import CardsSection from './views/CardsSection'
 import UsersSection from './views/UsersSection'
 
-function App (): JSX.Element {
-  const requestStatus = useFetchRequest<UsersApiResponse>(
-    'https://reqres.in/api/users?per_page=5'
-  )
-
-  console.log({ requestStatus })
+function App(): JSX.Element {
+  const requestStatus = useFetchRequest<UsersApiResponse>('https://reqres.in/api/users?per_page=5')
 
   return (
     <div className={styles.app__wrapper}>
