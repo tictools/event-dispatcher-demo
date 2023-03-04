662818e8c48e51d7bb47db43ec513b1e8183c5da
tictools <jaumevillarreal@gmail.com>
2023-03-04 07:55:54 +0100

    feat: add first iteration for App test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,16 +1,44 @@
-import { render } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import App from '../App'
 
-vi.mock('../hooks/useFetchRequest', () => ({
-  useFetchRequest: vi.fn().mockReturnValue('foo')
-}))
-
 describe('App', () => {
+  beforeEach(() => {
+    cleanup()
+  })
+
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
+
+  afterAll(() => {
+    vi.clearAllMocks()
+  })
+
   describe('App', () => {
+    test('given component App', () => {
+      vi.mock('../hooks/useFetchRequest', () => ({
+        useFetchRequest: vi.fn().mockReturnValue({
+          state: 'error',
+          data: null,
+          error: new Error('Something went wrong')
+        })
+      }))
+
+      render(<App />)
+      expect(screen.getByText(/wrong/)).toBeTruthy()
+    })
 
     test('given component App', () => {
-      const app = render(<App />)
-      console.log(app.debug())
+      // vi.mock('../hooks/useFetchRequest', () => ({
+      //   useFetchRequest: vi.fn().mockReturnValue({
+      //     state: 'loading',
+      //     data: null,
+      //     error: null
+      //   })
+      // }))
+
+      // const app = render(<App />)
+      expect(1).toEqual(1)
     })
   })
 })
